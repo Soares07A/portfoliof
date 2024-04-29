f26c39d96f0b1c544f16d806f708e95be15dd767
Soares07A <160130041+Soares07A@users.noreply.github.com>
2024-04-29 09:16:51 -0300

    eu2

--- teste.js
+++ teste.js
@@ -2,14 +2,21 @@ const cabecalho = document.querySelector('.cab');
 
 let ultimoScroll = 0;
 
-window.addEventListener('scroll', () => {
-  const atualScroll = window.pageOffset;
 
-  if (atualScroll > ultimoScroll) {
-    cabecalho.classList.add('fixado');
-  } else {
-    cabecalho.classList.remove('fixado');
-  }
+document.addEventListener("DOMContentLoaded",event=>{
+    window.addEventListener('scroll', () => {
+    const atualScroll = window.pageOffset;
+    
+      if (atualScroll > ultimoScroll) {
+        cabecalho.classList.add('fixado');
+      } else {
+        cabecalho.classList.remove('fixado');
+      }
+    
+      ultimoScroll = atualScroll;
+    });
 
-  ultimoScroll = atualScroll;
-});
+
+
+
+})
